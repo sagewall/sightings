d24233733f20533888887bf88d8bbab612f9050f
Sage Wall <swall@esri.com>
2024-07-01 12:37:09 -0600

    chore: add bundle analysis

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { bundleStats } from 'rollup-plugin-bundle-stats';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		bundleStats({
+			baseline: true
+		}),
+		sveltekit(),
+		visualizer({
+			brotliSize: true,
+			emitFile: true,
+			filename: 'stats.html',
+			gzipSize: true,
+			template: 'sunburst'
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
